05d1206f4a1b686757845b1bc60f61249aa21e98
Antonio Aleman <antonio.9714@gmail.com>
2019-10-10 17:32:02 -0500

    added login component

---
+++ src/Components/Login.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function Login() {
+    return(
+        <div className="App">
+      <head>
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
+      <link href="https://fonts.googleapis.com/css?family=Saira+Stencil+One&display=swap" rel="stylesheet"/>
+      </head>
+
+      <div className="background">
+        <div className="fContainer">
+          <span className="nameStyle">Contador </span><br />
+          <div className="loginInput">
+            <form>
+              <div className="form-group">
+                <label>Email</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
+                  placeholder="Ingresa tu correo" />
+                <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+              </div>
+              <div className="form-group">
+                <label for="exampleInputPassword1">Contraseña</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Contraseña" />
+              </div>
+              <button type="submit" className="btn-login">Entrar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
+}
