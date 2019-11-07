4dc7b1605f5ac791c8ea63e175ac6e677fc9eeea
Renato Sánchez <renato@emissary.mx>
2019-11-07 14:00:33 -0600

    agregando boton flotante que abre modal(en proceso)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,8 @@
 
 import React from 'react';
 import clsx from 'clsx';
-import { makeStyles, createMuiTheme }  from '@material-ui/core/styles';
-import {CssBaseline, Drawer, AppBar, Toolbar, List } from '@material-ui/core';
+import { makeStyles, createMuiTheme } from '@material-ui/core/styles';
+import { CssBaseline, Drawer, AppBar, Toolbar, List } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
@@ -15,165 +15,242 @@ import { mainListItems, secondaryListItems } from './ListItems';
 import Chart from './Chart';
 import Deposits from './Deposits';
 import Orders from './Orders';
+import Modal from '@material-ui/core/Modal';
+import Fab from '@material-ui/core/Fab';
+import AddIcon from '@material-ui/icons/Add';
+import TextField from '@material-ui/core/TextField';
+
+
+function rand() {
+	return Math.round(Math.random() * 20) - 10;
+}
+
+function getModalStyle() {
+	const top = 50 + rand();
+	const left = 50 + rand();
+
+	return {
+		top: `${top}%`,
+		left: `${left}%`,
+		transform: `translate(-${top}%, -${left}%)`,
+	};
+}
 
 const drawerWidth = 240;
 const useStyles = makeStyles(theme => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  subtitle: {
-    fontSize: '16px'
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing(7),
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9),
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  container: {
-    paddingTop: theme.spacing(4),
-    paddingBottom: theme.spacing(4),
-  },
-  paper: {
-    padding: theme.spacing(2),
-    display: 'flex',
-    overflow: 'auto',
-    flexDirection: 'column',
-  },
-  fixedHeight: {
-    height: 240,
-  },
+	root: {
+		display: 'flex',
+	},
+	toolbar: {
+		paddingRight: 24, // keep right padding when drawer closed
+	},
+	toolbarIcon: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		padding: '0 8px',
+		...theme.mixins.toolbar,
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+	},
+	appBarShift: {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	menuButton: {
+		marginRight: 36,
+	},
+	menuButtonHidden: {
+		display: 'none',
+	},
+	title: {
+		flexGrow: 1,
+	},
+	subtitle: {
+		fontSize: '16px'
+	},
+	drawerPaper: {
+		position: 'relative',
+		whiteSpace: 'nowrap',
+		width: drawerWidth,
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	drawerPaperClose: {
+		overflowX: 'hidden',
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		width: theme.spacing(7),
+		[theme.breakpoints.up('sm')]: {
+			width: theme.spacing(9),
+		},
+	},
+	appBarSpacer: theme.mixins.toolbar,
+	content: {
+		flexGrow: 1,
+		height: '100vh',
+		overflow: 'auto',
+	},
+	container: {
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(4),
+	},
+	paper: {
+		padding: theme.spacing(2),
+		display: 'flex',
+		overflow: 'auto',
+		flexDirection: 'column',
+	},
+	modal: {
+		position: 'absolute',
+		width: "50%",
+		backgroundColor: theme.palette.background.paper,
+		border: '2px solid #000',
+		padding: theme.spacing(2, 4, 3),
+	},
+	fixedHeight: {
+		height: 240,
+	},
+	fab: {
+		position: 'absolute',
+		bottom: theme.spacing(2),
+		right: theme.spacing(2),
+	}
 }));
 
 export default function Dashboard() {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
-  const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
+	const classes = useStyles();
+	const [modalStyle] = React.useState(getModalStyle);
+	const [open, setOpen] = React.useState(true);
+	const handleDrawerOpen = () => {
+		setOpen(true);
+	};
+	const handleDrawerClose = () => {
+		setOpen(false);
+	};
+	const fixedHeightPaper = clsx(classes.paper, classes.fixedHeight);
 
-  return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar position="absolute" color="primary" className={clsx(classes.appBar, open && classes.appBarShift)}>
-        <Toolbar className={classes.toolbar}>
-          <IconButton
-            edge="start"
-            color="green"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-            iFinanzas
+	const handleOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	return (
+		<div className={classes.root}>
+			<CssBaseline />
+			<AppBar position="absolute" color="primary" className={clsx(classes.appBar, open && classes.appBarShift)}>
+				<Toolbar className={classes.toolbar}>
+					<IconButton
+						edge="start"
+						color="green"
+						aria-label="open drawer"
+						onClick={handleDrawerOpen}
+						className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+						iFinanzas
           </Typography>
-          <Typography component="h5" variant="h6" color="inherit" noWrap className={classes.subtitle}>
-            Bienvenido, Patricio
+					<Typography component="h5" variant="h6" color="inherit" noWrap className={classes.subtitle}>
+						Bienvenido, Patricio
           </Typography>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        classes={{
-          paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-        }}
-        open={open}
-      >
-        <div className={classes.toolbarIcon}>
-          <IconButton onClick={handleDrawerClose}>
-            <ChevronLeftIcon />
-          </IconButton>
-        </div>
-        <Divider />
-        <List>{mainListItems}</List>
-        <Divider />
-        <List>{secondaryListItems}</List>
-      </Drawer>
-      <main className={classes.content}>
-        <div className={classes.appBarSpacer} />
-        <Container maxWidth="lg" className={classes.container}>
-          <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={12} md={8} lg={9}>
-              <Paper className={fixedHeightPaper}>
-                <Chart />
-              </Paper>
-            </Grid>
-            {/* Recent Deposits */}
-            <Grid item xs={12} md={4} lg={3}>
-              <Paper className={fixedHeightPaper}>
-                <Deposits />
-              </Paper>
-            </Grid>
-            {/* Recent Orders */}
-            <Grid item xs={12}>
-              <Paper className={classes.paper}>
-                <Orders />
-              </Paper>
-            </Grid>
-          </Grid>
-        </Container>
-      </main>
-    </div>
-  );
+				</Toolbar>
+			</AppBar>
+			<Drawer
+				variant="permanent"
+				classes={{
+					paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+				}}
+				open={open}
+			>
+				<div className={classes.toolbarIcon}>
+					<IconButton onClick={handleDrawerClose}>
+						<ChevronLeftIcon />
+					</IconButton>
+				</div>
+				<Divider />
+				<List>{mainListItems}</List>
+				<Divider />
+				<List>{secondaryListItems}</List>
+			</Drawer>
+			<main className={classes.content}>
+				<div className={classes.appBarSpacer} />
+				<Container maxWidth="lg" className={classes.container}>
+					<Grid container spacing={3}>
+						{/* Chart */}
+						<Grid item xs={12} md={8} lg={9}>
+							<Paper className={fixedHeightPaper}>
+								<Chart />
+							</Paper>
+						</Grid>
+						{/* Recent Deposits */}
+						<Grid item xs={12} md={4} lg={3}>
+							<Paper className={fixedHeightPaper}>
+								<Deposits />
+							</Paper>
+						</Grid>
+						{/* Recent Orders */}
+						<Grid item xs={12}>
+							<Paper className={classes.paper}>
+								<Orders />
+							</Paper>
+						</Grid>
+					</Grid>
+				</Container>
+				<Fab color="primary" aria-label="add" className={classes.fab} onClick={handleOpen}>
+					<AddIcon />
+				</Fab>
+			</main>
+			<div>
+				<Modal
+					aria-labelledby="simple-modal-title"
+					aria-describedby="simple-modal-description"
+					open={open}
+					onClose={handleClose}
+				>
+					<div style={modalStyle} className={classes.modal}>
+						<Paper className={classes.paper}>
+							<Grid container spacing={2}>
+								<Grid item xs={12} sm={6}>
+
+									<TextField
+										id="outlined-helperText"
+										label="Helper text"
+										defaultValue="Default Value"
+										className={classes.textField}
+										margin="normal"
+									/>
+									<TextField
+										id="outlined-helperText"
+										label="Helper text"
+										defaultValue="Default Value"
+										className={classes.textField}
+										margin="normal"
+									/>
+								</Grid>
+							</Grid>
+
+						</Paper>
+					</div>
+				</Modal>
+			</div>
+		</div>
+	);
 }
