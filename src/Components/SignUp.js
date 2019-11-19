6aac4775ee88c530c44a885e640e4c8acc1cd597
Antonio Aleman <antonio.9714@gmail.com>
2019-11-18 21:12:21 -0600

    added toast on SignUp

--- src/Components/SignUp.js
+++ src/Components/SignUp.js
@@ -8,9 +8,26 @@ import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
 import axios from 'axios';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import { amber, green } from '@material-ui/core/colors';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { Redirect } from 'react-router-dom';
 
 
+
+const variantIcon = {
+  success: CheckCircleIcon,
+  error: ErrorIcon,
+};
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -35,29 +52,97 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
 
+  success: {
+    backgroundColor: green[600],
+  },
+  error: {
+    backgroundColor: theme.palette.error.dark,
+  },
+  icon: {
+    fontSize: 20,
+  },
+  iconVariant: {
+    opacity: 0.9,
+    marginRight: theme.spacing(1),
+  },
+  message: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+}));
+function MySnackbarContentWrapper(props) {
+  const classes = useStyles();
+  const { className, message, onClose, variant, ...other } = props;
+  const Icon = variantIcon[variant];
+
+  return (
+    <SnackbarContent
+      className={clsx(classes[variant], className)}
+      aria-describedby="client-snackbar"
+      message={
+        <span id="client-snackbar" className={classes.message}>
+          <Icon className={clsx(classes.icon, classes.iconVariant)} />
+          {message}
+        </span>
+      }
+      action={[
+        <IconButton key="close" aria-label="close" color="inherit" onClick={onClose}>
+          <CloseIcon className={classes.icon} />
+        </IconButton>,
+      ]}
+      {...other}
+    />
+  );
+}
+MySnackbarContentWrapper.propTypes = {
+  className: PropTypes.string,
+  message: PropTypes.string,
+  onClose: PropTypes.func,
+  variant: PropTypes.oneOf(['error', 'info', 'success', 'warning']).isRequired,
+};
 export default function SignUp() {
 
   const classes = useStyles();
-//Falta ver si se quiere el nombre completo o solo uno solo
+  //Falta ver si se quiere el nombre completo o solo uno solo
   const [value, setValues] = React.useState("");
   const [email, setEmails] = React.useState("");
   const [password, setPasswords] = React.useState("");
+  const [open, setOpen] = React.useState(false);
+  const [open2, setOpen2] = React.useState(false);
+
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setOpen(false);
+    setOpen2(false);
+  };
 
   //const onChange = event => setValues(event.target.value);
-   const submitValue = () => {
+  const submitValue = () => {
     axios
-      .post('http://ifinanzas-api.herokuapp.com/users',{
-      name: value,
-      email: email,
-      password: password
+      .post('http://ifinanzas-api.herokuapp.com/users', {
+        name: value,
+        email: email,
+        password: password
       })
       .then(res => {
+
         console.log(res)
+
+        if (res.status == 200) {
+          //alert("Cuenta creada exitosamente");
+          setOpen(true);
+          setTimeout(function () { window.location.href = '/login'; }, 3000);
+          
+        };
       })
       .catch(err => {
-        console.log(err)
+        console.log("pene")
+        setOpen2(true);
+
       })
   }
 
@@ -66,11 +151,40 @@ export default function SignUp() {
     <Container component="main" maxWidth="xs">
       <CssBaseline />
       <div className={classes.paper}>
-
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={open}
+          autoHideDuration={6000}
+          onClose={handleClose}
+        >
+          <MySnackbarContentWrapper
+            onClose={handleClose}
+            variant="success"
+            message="Se creo la cuenta exitosamente!"
+          />
+        </Snackbar>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left',
+          }}
+          open={open2}
+          autoHideDuration={6000}
+          onClose={handleClose}
+        >
+          <MySnackbarContentWrapper
+            onClose={handleClose}
+            variant="error"
+            message="Hubo un error!"
+          />
+        </Snackbar>
         <Typography component="h1" variant="h5">
           Crear Cuenta
         </Typography>
-        <form  className={classes.form} noValidate>
+        <form className={classes.form} noValidate>
           <Grid container spacing={2}>
             <Grid item xs={6}>
               <TextField
@@ -112,7 +226,7 @@ export default function SignUp() {
                 variant="outlined"
                 required
                 fullWidth
-                label="Contraseña"
+                label="Contraseña (Minimo 8 caracteres)"
                 type="password"
                 id="password"
                 autoComplete="current-password"
@@ -144,7 +258,7 @@ export default function SignUp() {
           </Button>
           <Grid container justify="flex-end">
             <Grid item>
-              <Link href="#" variant="body2">
+              <Link href="/Login" variant="body2">
                 Ya tienes cuenta? Ingresa
               </Link>
             </Grid>
